e9134b4f99f22c961ed0cb288cb483c3e5d93a7e
VinceGusmini <vincent.gusmini.sio@gmail.com>
2022-01-29 20:15:48 +0100

    check if tirexo url + askTicket when fail

--- Program.cs
+++ Program.cs
@@ -1,30 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 
 namespace Suctionator
 {
     class Program
     {
+        private static string urlIssues = "https://github.com/VinceGusmini/Suctionator/issues";
         private static string urlInput;
+        private static string mediaName;
+        private static string mediaSeason;
+        private static List<string> uptoboxLinks = new List<string>();
 
         static void Main(string[] args)
         {
             urlInput = String.Empty;
+            mediaName = String.Empty; //[h3 p-2 : name]
+            mediaSeason = String.Empty;
             Console.WriteLine("Bienvenue sur Suctionator !");
 
-        Start:
+            // dossier LOG
+            // couleur ?
+            // mediaSeason handle both case
+
+
+
+            // 1
+            Start:
 
             AskInput();
 
             if (CheckUrl(urlInput)) //GET OK
             {
-                Console.WriteLine("Je tape les Pick-Ups !!!!!!!!");
+                Console.WriteLine("URL valid"); // TO EDIT / CHANGE
+
+                //GetUptoboxLinks(); // TODO 
+
+                //Console.WriteLine($"My cells have detected {uptoboxLinks} uptobox links to download for {mediaName}{mediaSeason}. Is that correct ?"); // TO EDIT / CHANGE                
+                Console.WriteLine($"My cells have detected 55 uptobox links to download for Toto saison 9. Is that correct ?"); // TO EDIT / CHANGE                
+                Console.WriteLine("'y' for YES || 'n' for NO :"); // TO EDIT / CHANGE
+                char response = Console.ReadLine()[0];
+
+                if (response == 'y') // OK
+                {
+                    Console.WriteLine("TOUT EST CARRE");
+                }
+
+                AskTicketIssue();
             }
             else
-            {
                 WrongInput();
-                goto Start;
-            }
+
+            goto Start;            
+        }
+
+        private static void GetUptoboxLinks()
+        {
+            throw new NotImplementedException();
         }
 
 
@@ -37,6 +69,12 @@ namespace Suctionator
         /// <returns></returns>
         private static Boolean CheckUrl(string urlInput)
         {
+            if (!urlInput.Contains("tirexo"))
+            {
+                Console.WriteLine("This is not Tirexo URL");
+                return false;
+            }
+
             Uri urlCheck = new Uri(urlInput);
 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlCheck);
@@ -45,10 +83,10 @@ namespace Suctionator
             HttpWebResponse response;
             try
             {
-                response = (HttpWebResponse)request.GetResponse();
-                return response.StatusCode == HttpStatusCode.OK;
+                response = (HttpWebResponse) request.GetResponse();
+                return response.StatusCode == HttpStatusCode.OK;                
             }
-            catch (Exception)
+            catch (Exception ex)
             {
                 return false;
             }
@@ -58,13 +96,19 @@ namespace Suctionator
         #region WriteLine
         private static void AskInput()
         {
-            Console.WriteLine("Merci d'indiquer l'url du média souhaité :");
+            Console.WriteLine("Enter the Tirexo URL of your choice :");
             urlInput = Console.ReadLine();
         }
 
         private static void WrongInput()
         {
-            Console.WriteLine("L'url n'est pas valide !");
+            Console.WriteLine("ERROR : Wrong input");
+        }
+
+        private static void AskTicketIssue()
+        {
+            Console.WriteLine($"Mission Failed... If possible go open a ticket issue with your requested URL in : {urlIssues}");
+            Console.WriteLine("In advance thank you for helping me improve this little exe :)");
         }
         #endregion
 
