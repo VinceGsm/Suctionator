beb401af80faf5a725a82da5f7e3949a807829d7
VinceGusmini <vincent.gusmini.sio@gmail.com>
2022-02-09 12:27:57 +0100

    Extract infos ok

--- Program.cs
+++ Program.cs
@@ -1,23 +1,25 @@
 ﻿using HtmlAgilityPack;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Net;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Suctionator
 {
     class Program
-    {
-        // https://www2.tirexo.work/telecharger-series/617578-le-bureau-des-legendes-saison-1-Blu-Ray%201080p-French.html
+    {        
         private static string urlIssues = "https://github.com/VinceGusmini/Suctionator/issues";
         private static string urlInput;
         private static string mediaName;
         private static string mediaSeason;
+        private static int countTotalLinks;
         private static List<string> uptoboxLinks = new List<string>();
         private static ILogger log;
+        private static HtmlDocument htmlDoc; 
+        private static HtmlNode nodeLinks; 
 
         static void Main(string[] args)
         {
@@ -37,9 +39,14 @@ namespace Suctionator
             //log.LogCritical("Critical ");
             #endregion
 
-            urlInput = String.Empty;
-            mediaName = String.Empty; //[h3 p-2 : name]
+            htmlDoc = new HtmlDocument();
+            htmlDoc.OptionFixNestedTags = true;
+            //urlInput = String.Empty; 
+            urlInput = "https://www2.tirexo.art/animes/674759-l-attaque-des-titans-WEB-DL%201080p-VOSTFR.html";
+            //urlInput = "https://www2.tirexo.art/telecharger-series/617584-le-bureau-des-legendes-saison-3-Blu-Ray%201080p-French.html";           
+            mediaName = String.Empty;
             mediaSeason = String.Empty;
+            countTotalLinks = 0;
             Console.WriteLine("Bienvenue sur Suctionator !");
 
             // 1
@@ -51,9 +58,11 @@ namespace Suctionator
             {
                 Console.WriteLine("URL valid"); // TO EDIT / CHANGE
 
+                ExtractInfos();
+
                 GetUptoboxLinks(); // TODO 
 
-                Console.WriteLine($"My cells have detected {uptoboxLinks} uptobox links to download for {mediaName}{mediaSeason}. Is that correct ?"); // TO EDIT / CHANGE                                         
+                Console.WriteLine($"My cells have detected {uptoboxLinks.Count} uptobox links to download for {mediaName} (Saison {mediaSeason}). Is that correct ?"); // TO EDIT / CHANGE                                         
                 Console.WriteLine("'y' for YES || 'n' for NO :"); // TO EDIT / CHANGE
                 char response = Console.ReadLine()[0];
 
@@ -72,7 +81,12 @@ namespace Suctionator
 
         private static void GetUptoboxLinks()
         {
-            throw new NotImplementedException();
+            //var listtmp = htmlDoc.DocumentNode.Descendants("table").ToList();                
+            var tmpUptoboxTable = htmlDoc.DocumentNode.Descendants("table").FirstOrDefault(x => x.XPath ==
+                "/html[1]/body[1]/div[3]/div[4]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/table[1]");
+            nodeLinks = tmpUptoboxTable.ChildNodes.FirstOrDefault(child => child.Name == "tbody");
+
+            var toto = "";
         }
 
 
@@ -95,7 +109,7 @@ namespace Suctionator
             try
             {
                 string htmlStr = await httpClient.GetStringAsync(urlInput);
-                HtmlDocument htmlDoc = new HtmlDocument();
+                
                 htmlDoc.LoadHtml(htmlStr);
 
                 if(!String.IsNullOrEmpty(htmlDoc.ParsedText))
@@ -113,13 +127,89 @@ namespace Suctionator
                 return false;
             }
         }
+
+
+        /// <summary>
+        /// Extract media name + media season
+        /// </summary>
+        private static void ExtractInfos()
+        {
+            try
+            {                                
+                var tmpLi = htmlDoc.DocumentNode.Descendants("li").ToList();
+                var tmpH3 = htmlDoc.DocumentNode.Descendants("h3").ToList();
+                var tmpDiv = htmlDoc.DocumentNode.Descendants("div").ToList();
+
+                var tmpMediaName = tmpH3.First().InnerText;
+                var tmpSeasonNode = tmpLi.FirstOrDefault(x => x.OuterHtml.StartsWith("<li><strong>Saison</strong> :"));
+
+                mediaName = CleanHtmlCode(tmpMediaName);
+                mediaSeason = tmpSeasonNode.InnerHtml.Substring(26, 1);
+
+                //var testo = tmpDiv.FirstOrDefault(x => x.XPath == "/html[1]/body[1]/div[3]/div[4]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[3]/div[1]");
+                // xpath potentiel autre node links :              /html[1]/body[1]/div[3]/div[4]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[3]/div[1]               
+
+                //Console.WriteLine($"TOTAL = "+tmpDiv.Count);
+                //int i = 0;
+                //foreach (var node2merde in tmpDiv)
+                //{                    
+                //    //Console.WriteLine($"#{i} : "+ node2merde.OuterHtml);
+                //    Console.WriteLine("--------");
+                //    Console.WriteLine($"#{i} : " + node2merde.InnerText);
+                //    i++;
+                //}
+
+                //List<string> ids = new List<string>();
+                //foreach (var node in htmlDoc.SelectNodes("//div/@id"))
+                //{
+                //    ids.Add(node.InnerText);
+                //}
+
+                //var htmlNodes = htmlDoc.DocumentNode.SelectNodes("//dataTables_info");
+                //var htmlNodessss = htmlDoc.DocumentNode.SelectNodes("//DataTables_Table_0_info"); 
+                //var htmlNodezdzqdssss = htmlDoc.DocumentNode.SelectNodes("//row");
+
+                //var tmpRow = .DocumentNode.Descendants("row").ToList();
+
+                //var testt = htmlDoc.DocumentNode.SelectNodes("//div[contains(@class,'col-sm-12 col-md-5')]");
+
+                //var divWithAttributes = tmpDiv.Where(x => x.Attributes.Count >= 1).ToList();
+                //var divWithOutAttributes = tmpDiv.Where(x => x.Attributes.Count == 0).ToList();
+
+                //var testtt = htmlDoc.DocumentNode.SelectNodes("//div[contains(@class,'dataTables_info.DataTables_Table_0_info')]");
+                //var testttt = htmlDoc.DocumentNode.SelectNodes("//div[contains(@class,'dataTables_info')]");
+                //var testdttt = htmlDoc.DocumentNode.SelectNodes("//div[contains(@id,'DataTables_Table_0_info')]");
+
+                //var id = "DataTables_Table_0_info";
+                //var query = $"//manifest/item[@id='{id}']";
+                //HtmlNode node = htmlDoc.DocumentNode.SelectSingleNode(query);
+
+                //var tmpSpan = htmlDoc.DocumentNode.Descendants("span").ToList();
+
+                //var tesBIS = htmlDoc.DocumentNode.SelectNodes("//span[contains(@class,'masha_index masha_index112')]");
+
+                ////var titi = htmlDoc.DocumentNode.Descendants("DataTables_Table_0_info");
+                //var ezrtyj = htmlDoc.DocumentNode.Descendants("col-sm-12 col-md-5");
+                //var DONC  = tmpDiv.FirstOrDefault(w => w.OuterHtml.Contains("dataTables_info"));
+            }
+            catch (Exception ex)
+            {
+                log.LogCritical("ExtractInfos: " + ex.Message);
+            }
+        }
+
+        private static string CleanHtmlCode(string tmpSeasonNumber)
+        {
+            tmpSeasonNumber = tmpSeasonNumber.Replace("&#039;", "'");
+            return tmpSeasonNumber;
+        }
         #endregion
 
         #region Console
         private static void AskInput()
         {
             Console.WriteLine("Enter the Tirexo URL of your choice :");
-            urlInput = Console.ReadLine();
+            //urlInput = Console.ReadLine();
         }
 
         private static void WrongInput()
