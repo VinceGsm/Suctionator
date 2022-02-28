953007018fe4e77a259f8babd4ffb264eb7368e7
VinceGusmini <vincent.gusmini.sio@gmail.com>
2022-02-28 12:26:59 +0100

    no log + DL list + autoMod : V2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using HtmlAgilityPack;
-using Microsoft.Extensions.Logging;
+using Microsoft.Win32;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
@@ -7,7 +7,10 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Reflection;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Terminal.Gui;
 
@@ -15,42 +18,43 @@ namespace Suctionator
 {
     class Program
     {
-        private static string _version = "Version 1.0.2";
-        private static string urlIssues = "https://github.com/VinceGusmini/Suctionator/issues";         
+        private static string _version = "Version " + Assembly.GetEntryAssembly()
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+        private static string urlIssues = "https://github.com/VinceGusmini/Suctionator/issues";                        
+        private static string _pathDl;
+        private static string _uptoboxToken;        
+        private static int numLastEpisode = 0;
+        private static int countTotalLinks = 0;
+        private static Label labelAutoDl;
+        private static Stopwatch stopWatch;        
+        private static TextField entryLink;
+        private static HtmlDocument htmlDoc;
+        private static TextField entryGetToken;
+        private static bool _isAutoDlOn = false;
         private static string urlInput = string.Empty;
-        private static string resultUptobox = string.Empty;
-        private static string baseUrlInput = string.Empty;
         private static string mediaName = string.Empty;
         private static string mediaSeason = string.Empty;
-        private static int countTotalLinks = 0;
-        private static int numLastEpisode = 0;
-        private static List<string> uptoboxLinks = new List<string>();
+        private static string baseUrlInput = string.Empty;
+        private static string resultUptobox = string.Empty;
         private static List<string> pubLinks = new List<string>();
-        private static ILogger log;
-        private static HtmlDocument htmlDoc;
-        private static string _uptoboxToken;         
-        private static TextField entryGetToken;
-        private static TextField entryLink;
-        private static Stopwatch stopWatch;
+        private static List<string> uptoboxLinks = new List<string>();
 
 
         [STAThread]
         static async Task Main(string[] args)
         {
-            #region Log init
-            using var loggerFactory = LoggerFactory.Create(builder =>
-            {
-                builder
-                    .AddFilter("Microsoft", Microsoft.Extensions.Logging.LogLevel.Warning)
-                    .AddFilter("System", Microsoft.Extensions.Logging.LogLevel.Warning)
-                    .AddFilter("NonHostConsoleApp.Program", Microsoft.Extensions.Logging.LogLevel.Debug)
-                    .AddConsole();
-            });
-            log = loggerFactory.CreateLogger<Program>();            
-            #endregion
+            _uptoboxToken = Environment.GetEnvironmentVariable("Suctionator_Token");
+            _pathDl = Environment.GetEnvironmentVariable("Suctionator_Path");
 
-            _uptoboxToken = Environment.GetEnvironmentVariable("Uptobox_Token");
-            stopWatch = new Stopwatch();
+            if (string.IsNullOrEmpty(_pathDl))
+            {
+                _pathDl = Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders",
+                    "{374DE290-123F-4565-9164-39C4925E467B}", string.Empty).ToString();
+            }
+            else if (_pathDl == @"E:\Downloads")
+                _isAutoDlOn = true; //Home
+            
+            stopWatch = new Stopwatch();        
 
             // GUI
             Application.Init();
@@ -67,9 +71,12 @@ namespace Suctionator
                     new MenuItem ("Suctionator", "", () => SetupHome(homeWindow)),
                     new MenuItem ("Résultat", "", () => GiveResult()),
                     new MenuItem ("Quitter", "", () => { if (Quit()) Application.RequestStop(); } )
+                }),  
+                new MenuBarItem ("Téléchargement", new MenuItem [] {
+                    new MenuItem ("Récupération automatique des résultats", "", () => RequestDownload())
                 }),
-                new MenuBarItem ("Automatique", new MenuItem [] {
-                    new MenuItem ("Special One Piece", "", () => Soon())
+                new MenuBarItem ("Special", new MenuItem [] {
+                    new MenuItem ("One Piece", "", () => Soon())
                 }),
                 new MenuBarItem ("", new MenuItem [] {}),
                 new MenuBarItem ("", new MenuItem [] {})
@@ -80,13 +87,19 @@ namespace Suctionator
                 menuBarItem.SetValue(new MenuBarItem("Uptobox", new MenuItem[] {
                     new MenuItem ("Connexion", "", () => UptoboxConnexion())                   
                     }
-                ), 2);
-            }
-
-            menuBarItem.SetValue(new MenuBarItem("Help", new MenuItem[] {                    
-                    new MenuItem ("Get help", "", () => GetHelp() ) 
+                ), 3);
+                menuBarItem.SetValue(new MenuBarItem("Help", new MenuItem[] {
+                    new MenuItem ("Get help", "", () => GetHelp() )
                     }
-            ), 3);
+                ), 4);
+            }
+            else
+            {
+                menuBarItem.SetValue(new MenuBarItem("Help", new MenuItem[] {
+                    new MenuItem ("Get help", "", () => GetHelp() )
+                    }
+                ), 3);
+            }
 
             var menuBar = new MenuBar(menuBarItem);            
             
@@ -98,12 +111,16 @@ namespace Suctionator
         }
 
 
-        #region Front_end
+        #region Front_end + Gui Action
         private static void Soon()
         {
-            MessageBox.ErrorQuery(50, 7, "Prochainement", "Cette fonctionnalité sera disponible dans la Version 2.0", "J'attends");
+            MessageBox.ErrorQuery(50, 7, "Prochainement", "Cette fonctionnalité sera disponible dans la Version 2.1", "J'attends");
         }
 
+        /// <summary>
+        /// Automatically resize without manual intervention
+        /// </summary>
+        /// <returns></returns>
         private static Window CreateHomeWindow()
         {
             return new Window(_version)
@@ -111,7 +128,6 @@ namespace Suctionator
                 X = 0,
                 Y = 1, // Leave one row for the toplevel menu
 
-                // automatically resize without manual intervention
                 Width = Dim.Fill(-5),
                 Height = Dim.Fill(-5)
             };
@@ -156,7 +172,8 @@ namespace Suctionator
 
         private static void SetupHome(View home)
         {
-            var label = new Label("(Tirexo) URL de la saison :")
+            #region Main Input
+            var labelInput = new Label("URL Tirexo de la saison :")
             {
                 X = Pos.Center(),
                 Y = Pos.Center() - 10,
@@ -164,49 +181,80 @@ namespace Suctionator
                 Height = 1
             };
 
-            entryLink = new TextField("")            
+            entryLink = new TextField("")
             {
-                X = label.X,
-                Y = label.Y + 2,
+                X = labelInput.X,
+                Y = labelInput.Y + 2,
                 Width = 110,
                 Height = 1
             };
 
-            var btn = new Button("GO !")
+            var btnInput = new Button("GO !")
             {
-                X = entryLink.X,
+                X = entryLink.X - 10,
                 Y = entryLink.Y + 2,
                 Width = 10,
                 Height = 1
             };
 
-            btn.Clicked += new Action(GoClicked);
+            btnInput.Clicked += new Action(GoClicked);
+            #endregion
 
-            home.Add(label, entryLink, btn);
+            #region "Auto" Input
+            labelAutoDl = new Label($"Mode Auto Download : {_isAutoDlOn}")
+            {
+                X = btnInput.X - 5,
+                Y = btnInput.Y + 5,
+                Width = 30,
+                Height = 1
+            };
+            
+            labelAutoDl.Clicked += new Action(AutoDLClicked);            
+            #endregion
+
+            home.Add(labelInput, entryLink, btnInput, labelAutoDl);            
         }
 
         private static void GoClicked()
-        {
+        {            
             ResetResult();
             stopWatch.Start();
 
             urlInput = entryLink.Text.ToString();
-            if (CheckUrlAsync(urlInput).Result)
-            {                
-                ExtractInfos();
-                MessageBox.Query(50, 5, "1/3 : Extracting data done", $"Aspiration pour {mediaName} (Saison {mediaSeason})", "Suivant");
+            if (CheckUrl(urlInput))
+            {
+                if (_isAutoDlOn)
+                {
+                    ExtractInfos();
+                    MessageBox.Query(50, 5, "MODE AUTO : ON", $"Aspiration + Download pour {mediaName} (Saison {mediaSeason})", "Suivant");
+                    MessageBox.ErrorQuery(50, 5, "MODE AUTO : ON", "NE TOUCHEZ PLUS A RIEN JUSQU'AU PROCHAIN MESSAGE", "Compris");
+                    GetPubLinks();
+                    GetUptoboxLinks();
+                    DownloadProcess();
+                }
+                else
+                {
+                    ExtractInfos();
+                    MessageBox.Query(50, 5, "1/3 : Extracting data done", $"Aspiration pour {mediaName} (Saison {mediaSeason})", "Suivant");
 
-                GetPubLinks();
-                MessageBox.ErrorQuery(50, 5, "Links 1/2 : pub done", "NE TOUCHEZ PLUS A RIEN JUSQU'AU PROCHAIN MESSAGE", "Compris");
+                    GetPubLinks();
+                    MessageBox.ErrorQuery(50, 5, "Links 1/2 : pub done", "NE TOUCHEZ PLUS A RIEN JUSQU'AU PROCHAIN MESSAGE", "Compris");
 
-                GetUptoboxLinks();
-                stopWatch.Stop();
-                MessageBox.Query(50, 5, "Links 2/2 : Uptobox", $"Etape 3/3 ! Aspiration effectuée en {stopWatch.Elapsed.Minutes}m", "OK");
-                MessageBox.Query(50, 5, "Informations", $"Une liste de {uptoboxLinks.Count} (sur {countTotalLinks} analysé)" +
-                    " liens Uptobox vous attendent dans le menu \"Résultat\" ", "Super !");                                                           
+                    GetUptoboxLinks();
+                    stopWatch.Stop();
+                    MessageBox.Query(50, 5, "Links 2/2 : Uptobox", $"Etape 3/3 ! Aspiration effectuée en {stopWatch.Elapsed.Minutes}m", "OK");
+                    MessageBox.Query(50, 5, "Informations", $"Une liste de {uptoboxLinks.Count} (sur {countTotalLinks} analysé)" +
+                        " liens Uptobox vous attendent dans le menu \"Résultat\" ", "Super !");
+                }
             }
         }
 
+        private static void AutoDLClicked()
+        {
+            _isAutoDlOn = !_isAutoDlOn;
+            labelAutoDl.Text = $"Mode Auto Download : {_isAutoDlOn}";
+        }
+        
         private static void GiveResult()
         {
             if (!string.IsNullOrEmpty(resultUptobox))
@@ -218,19 +266,50 @@ namespace Suctionator
                     MessageBox.ErrorQuery(50, 5, "ERROR", "Impossible de récupéré les résultats", "Ok");
             }
             else
-                MessageBox.ErrorQuery(50, 5, "ERROR", "Aucun résultat à récupérer", "Ok");
+                MessageBox.ErrorQuery(50, 5, "Alerte", "Aucun résultat à récupérer", "Ok");
         }
 
-        private static void ResetResult()
+        private static void RequestDownload()
         {
-            resultUptobox = string.Empty;
+            if (string.IsNullOrEmpty(_uptoboxToken))
+            {
+                MessageBox.ErrorQuery(50, 7, "Alerte", "Impossible de télécharger sans Token Uptobox", "Ok");
+            }
+            else if (uptoboxLinks.Count == 0)
+            {
+                MessageBox.ErrorQuery(50, 5, "Alerte", "Aucun résultat à récupérer", "Ok");
+            }
+            else
+            {
+                MessageBox.Query(50, 5, "Download in progress", "Merci de laisser tournée en fond l'application.", "Ok");
+                MessageBox.Query(50, 5, "Download in progress", "Un message vous donnera le nombre d'épisode téléchargé ou si une erreur survient", "Ok");
+
+                DownloadProcess();
+            }
+        }
+
+        private static void DownloadProcess()
+        {
+            stopWatch.Restart();
+            stopWatch.Start();
+
+            int countSucceedDL = DownloadResult();
+            if (countSucceedDL > 1)
+            {
+                stopWatch.Stop();
+                MessageBox.Query(50, 5, "Terminé", $"Un total de {countSucceedDL} épisodes téléchargés sur {uptoboxLinks.Count} dans vos résultats " +
+                    $"(en {stopWatch.Elapsed.Minutes}m)", "Ok");
+            }
+            else
+                MessageBox.ErrorQuery(50, 5, "ERROR", "RequestDownload failed, please contact Vince on Github or Discord", "Ok");
         }
 
         private static void GetHelp()
         {
             bool success = Clipboard.TrySetClipboardData(urlIssues);
             if (success)
-                MessageBox.Query(50, 5, "Url du projet récupéré", "En cas de problème merci de créer un post sur Github: le lien a directement été collé dans votre clipbopard", "Ok");
+                MessageBox.Query(50, 5, "Url du projet récupéré", "En cas de problème merci de créer un post sur Github: " +
+                    "le lien a directement été collé dans votre clipbopard", "Ok");
             else
                 MessageBox.ErrorQuery(50, 5, "ERROR", "Impossible de récupéré le lien du projet", "Ok");
 
@@ -244,14 +323,74 @@ namespace Suctionator
         }
         #endregion
 
-
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         #region Back_end    
+        /// <summary>
+        /// Use token Uptobox to download all the links in the result var
+        /// </summary>
+        /// <returns>number of dowloaded episode</returns>
+        private static int DownloadResult()
+        {
+            int res = 0;         
+
+            foreach (var link in uptoboxLinks)
+            {
+                var isDlSucceed = DownloadLink(link);
+                if (isDlSucceed)
+                    res++;
+            }
+            return res;
+        }
+
+        private static bool DownloadLink(string link)
+        {
+            string codeToken = GetCodeToken(link);
+            string requestTokenUrl = $"https://uptobox.com/api/link?token={_uptoboxToken}&file_code={codeToken}";
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    // Get the final url to DL with the Token
+                    var responseToken = client.DownloadString(requestTokenUrl);
+                    var responseResult = JsonSerializer.Deserialize<ResponseWaitToken>(responseToken);
+                    string fileName =_pathDl + '\\' + GetFileName(responseResult.data.dlLink);
+                    //DL 
+                    client.DownloadFile(responseResult.data.dlLink, fileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.ErrorQuery(50, 7, "Error", $"{ex.Message}", "Ok");                
+                return false;
+            }
+            return true;
+        }
+
+        private static string GetCodeToken(string link)
+        {
+            var tmp = link.Split("/");
+            var bonsoir = tmp.Last();
+
+            if (bonsoir.Contains('?'))
+            {
+                tmp = bonsoir.Split("?");
+                bonsoir = tmp.First();
+            }            
+            return bonsoir;                                    
+        }
+
+        private static string GetFileName(string dlLink)
+        {
+            var tmp = dlLink.Split("/");
+            return tmp.Last().Replace(@"%20", " ");
+        }
+
         /// <summary>
         /// Request the target url to see if it's online
         /// </summary>
         /// <param name="urlInput"></param>
         /// <returns></returns>
-        private static async Task<bool> CheckUrlAsync(string urlInput)
+        private static bool CheckUrl(string urlInput)
         {
             if (!urlInput.Contains("tirexo"))
             {
@@ -266,17 +405,16 @@ namespace Suctionator
                 
                 htmlDoc.LoadHtml(htmlStr);
 
-                if(!String.IsNullOrEmpty(htmlDoc.ParsedText))                                    
+                if (!string.IsNullOrEmpty(htmlDoc.ParsedText))
                     return true;
-
-                MessageBox.ErrorQuery(50, 7, "Error", "Impossible d'accéder à la page", "Ok");
-                return false;
+                else
+                    throw new Exception();
             }
             catch (Exception ex)
             {
-                log.LogCritical(ex.Message);
-                return false;
+                MessageBox.ErrorQuery(50, 7, "Error", "Impossible d'accéder à la page", "Ok");                
             }
+            return false;
         }
 
         /// <summary>
@@ -303,7 +441,7 @@ namespace Suctionator
             }
             catch (Exception ex)
             {
-                log.LogCritical("ExtractInfos: " + ex.Message);
+                MessageBox.ErrorQuery(50, 7, "Error", $"ExtractInfos failed : {ex.Message}", "Ok");
             }
         }
 
@@ -341,8 +479,7 @@ namespace Suctionator
                 }
                 catch (Exception ex)
                 {
-                    browserDriver.Quit();
-                    log.LogCritical(ex.Message);
+                    browserDriver.Quit();                    
                 }
             }
             resultUptobox = string.Join(Environment.NewLine, uptoboxLinks);
@@ -365,9 +502,8 @@ namespace Suctionator
             {
                 int numEpisodeTarget = 0;
                 bool firstTime = true;
-                          
-                var tmpPubTable = htmlDoc.DocumentNode.Descendants("table").FirstOrDefault(x => x.XPath ==
-                    "/html[1]/body[1]/div[3]/div[4]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/table[1]");
+
+                var tmpPubTable = htmlDoc.DocumentNode.Descendants("table").First();
                 HtmlNode nodePubLinks = tmpPubTable.ChildNodes.FirstOrDefault(child => child.Name == "tbody");
 
                 var lstTr = nodePubLinks.ChildNodes.ToList();
@@ -397,8 +533,14 @@ namespace Suctionator
             }
             catch (Exception ex)
             {
-                log.LogCritical("GetUptoboxLinks: " + ex.Message);
+                MessageBox.ErrorQuery(50, 7, "Error", $"GetPubLinks failed : {ex.Message}", "Ok");
             }
+        }
+
+        private static void ResetResult()
+        {
+            resultUptobox = string.Empty;
+            uptoboxLinks.Clear();
         }
 
         private static int RecoverNumEpisode(HtmlNode node)
@@ -430,11 +572,5 @@ namespace Suctionator
             htmlDoc.OptionFixNestedTags = true;
         }
         #endregion
-
-        ///////////////////////////////////////////////////////////////////////////////////////
-        /*
-        "https://uptobox.com/api/link?token=[USR_TOKEN]&file_code=[FILE_CODE]";
-        */
-        /////////////////////////////////////////////////////////////////////////////////// 
     }
 }
---
+++ ResponseWaitToken.cs
@@ -0,0 +1,15 @@
+﻿namespace Suctionator
+{
+    public class ResponseWaitToken
+    {
+        public int statusCode { get; set; }
+        public string message { get; set; }
+        
+        public Data data { get; set; }
+    }
+
+    public class Data
+    {
+        public string dlLink { get; set; }
+    }
+}
